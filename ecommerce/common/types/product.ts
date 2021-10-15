76b08e592a20ac8a4bb829edd2d883a4f9bb0d53
Michael Nelson <revnelson@gmail.com>
2021-10-15 18:47:37 +0000

    Add product and cart types.

--- ecommerce/common/types/product.ts
+++ ecommerce/common/types/product.ts
@@ -8,6 +8,28 @@ export interface ProductPrice {
   currencyCode: "USD" | "EUR" | string
 }
 
+export interface ProductOptionValues {
+  label: string
+  hexColor?: string
+}
+
+export interface ProductOption {
+  id: string
+  displayName: string
+  values: ProductOptionValues[]
+}
+
+export interface ProductVariant {
+  id: string
+  name: string
+  sku: string
+  image?: ProductImage
+  requiresShipping: boolean
+  price: number
+  listPrice: number
+  options: ProductOption[]
+}
+
 export interface Product {
   id: string
   name: string
@@ -16,4 +38,6 @@ export interface Product {
   path: string
   images: ProductImage[]
   price: ProductPrice
+  options: ProductOption[]
+  variants: ProductVariant[]
 }
