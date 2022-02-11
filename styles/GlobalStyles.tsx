057643f2555af7473ed8f982a9427a435d8028ba
Michael Nelson <revnelson@gmail.com>
2022-02-12 04:06:14 +0800

    Update fallback fonts.

--- styles/GlobalStyles.tsx
+++ styles/GlobalStyles.tsx
@@ -1,6 +1,5 @@
 import React from "react"
 import { css, Global } from "@emotion/react"
-import tw, { theme, GlobalStyles as BaseStyles } from "twin.macro"
 
 const fontsCDN = `${process.env.NEXT_PUBLIC_CDN_BASE_URL}/fonts`
 export const customStyles = css`
@@ -19,12 +18,13 @@ export const customStyles = css`
   }
 
   body {
-    -webkit-tap-highlight-color: ${theme`colors.blue.main`};
+    -webkit-tap-highlight-color: #5375a0;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
 
     .font-family {
-      font-family: Montserrat, sans-serif;
+      font-family: Montserrat, Verdana, sans-serif;
+      font-display: swap;
     }
   }
 
@@ -38,41 +38,12 @@ export const customStyles = css`
     }
   }
 
-  /* .responsivePadding {
-    padding-top: 52.25%;
-    // medium
-    @media (min-width: 768px) {
-      padding-top: 40%;
-    }
-    // large
-    @media (min-width: 1024px) {
-      padding-top: 32.5%;
-    }
-  }
-
-  .responsivePadding-thin {
-    padding-top: 47%;
-    // medium
-    @media (min-width: 768px) {
-      padding-top: 15%;
-    }
-    // large
-    @media (min-width: 1024px) {
-      padding-top: 15%;
-    }
-  }
-
-  .padding16x9 {
-    padding-top: 56.25%;
-  } */
-
   .danger {
     ul {
       list-style: inside disc;
       margin-left: 1rem /** 16px */;
-      font-size: 0.875rem /** 14px */;
+      font-size: 1rem /** 14px */;
       line-height: 1.25rem /** 20px */;
-      ${tw`text-base`}
     }
   }
 
@@ -86,7 +57,7 @@ export const customStyles = css`
 
 const GlobalStyles = () => (
   <>
-    <BaseStyles />
+    {/* <BaseStyles /> */}
     <Global styles={customStyles} />
   </>
 )
