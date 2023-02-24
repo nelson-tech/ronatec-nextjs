d45d0455cd7a431ce0014b387bdd1d3ea826d96f
Michael Nelson <revnelson@gmail.com>
2023-02-24 05:30:16 +0000

    Add favicons and webmanifest.

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,8 +56,14 @@ export const metadata = {
     template: "%s",
   },
   icons: {
-    icon: "/favicon.png",
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: "/apple-touch-icon.png",
   },
+  manifest: "/site.webmanifest",
   robots: {
     index: true,
     follow: true,
