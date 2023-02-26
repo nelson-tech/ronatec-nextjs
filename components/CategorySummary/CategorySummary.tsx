a2648e61269dbb6f92ff080010c9b52e89b243d4
Michael Nelson <revnelson@gmail.com>
2023-02-26 20:10:39 +0000

    Fix category summary styling issues.

--- components/CategorySummary/CategorySummary.tsx
+++ components/CategorySummary/CategorySummary.tsx
@@ -44,7 +44,10 @@ const Summary = ({ category, productRef, main }: PropsType) => {
             </span>
           </div>
           <div
-            className="mt-4 py-2 text-base text-gray-500 prose max-w-none prose-sm prose-a:text-accent hover:prose-a:text-highlight prose-p:mt-4"
+            className="mt-4 py-2 text-base text-gray-500 
+            prose max-w-none prose-sm 
+            prose-headings:border-t prose-headings:pt-2 prose-headings:mt-4 
+            prose-a:text-accent hover:prose-a:text-highlight prose-p:mt-4"
             dangerouslySetInnerHTML={{ __html: category?.description ?? "" }}
           />
         </div>
