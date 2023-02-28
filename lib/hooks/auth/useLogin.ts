6760c1b95cc771ca8f60b7301075a4c13875c3e5
Michael Nelson <revnelson@gmail.com>
2023-02-28 06:40:43 +0000

    Update useLogin to handle errors.

--- lib/hooks/auth/useLogin.ts
+++ lib/hooks/auth/useLogin.ts
@@ -9,13 +9,13 @@ import {
 import useStore from "@lib/hooks/useStore"
 import { AUTH_TOKEN_KEY, REFRESH_TOKEN_KEY } from "@lib/constants"
 import setCookie from "@lib/utils/setCookie"
+import { useCallback } from "react"
 
 const useLogin = () => {
-  const { setAlert, setCustomer, setLoggedIn, setLoginError } = useStore(
+  const { setAlert, setCustomer, setLoggedIn } = useStore(
     (state) => ({
       setCustomer: state.auth.setCustomer,
       setLoggedIn: state.auth.setLoggedIn,
-      setLoginError: state.auth.setLoginError,
       setAlert: state.alert.setAlert,
     }),
     shallow
@@ -23,48 +23,52 @@ const useLogin = () => {
 
   const client = getClient()
 
-  const login = async ({ input }: LoginUserMutationVariables) => {
-    await client
-      .request(LoginUserDocument, { input })
-      .then(async (data) => {
-        if (data) {
-          const { login } = data
-          if (login?.authToken && login.refreshToken) {
-            const customer = login.customer
+  const login = useCallback(
+    async ({ input }: LoginUserMutationVariables) => {
+      await client
+        .request(LoginUserDocument, { input })
+        .then(async (data) => {
+          if (data) {
+            const { login } = data
+            if (login?.authToken && login.refreshToken) {
+              const customer = login.customer
 
-            // Set authToken in client
+              // Set authToken in client
+              client.setHeader("Authorization", `Bearer ${login.authToken}`)
 
-            client.setHeader("Authorization", `Bearer ${login.authToken}`)
+              // Set customer in store
+              setCustomer(customer as Customer)
 
-            // Set customer in store
-            setCustomer(customer as Customer)
+              // Set cookies
+              setCookie(AUTH_TOKEN_KEY, login.authToken)
+              setCookie(REFRESH_TOKEN_KEY, login.refreshToken)
 
-            // Set cookies
-            setCookie(AUTH_TOKEN_KEY, login.authToken)
-            setCookie(REFRESH_TOKEN_KEY, login.refreshToken)
-
-            setAlert({
-              open: true,
-              kind: "success",
-              primary: `Welcome back${
-                (customer?.firstName || customer?.lastName) && ","
-              }${customer?.firstName && ` ${customer.firstName}`}${
-                customer?.lastName && ` ${customer.lastName}`
-              }!`,
-              secondary: "You are now logged in.",
-            })
-            setLoggedIn(true)
+              setAlert({
+                open: true,
+                kind: "success",
+                primary: `Welcome back${
+                  (customer?.firstName || customer?.lastName) && ","
+                }${customer?.firstName && ` ${customer.firstName}`}${
+                  customer?.lastName && ` ${customer.lastName}`
+                }!`,
+                secondary: "You are now logged in.",
+              })
+              setLoggedIn(true)
+            }
           }
-        }
-      })
-      .catch((error) => {
-        console.warn("Error logging in", error)
+        })
+        .catch((error) => {
+          console.warn("Error logging in", error)
 
-        if (error.message.includes("invalid")) {
-          setLoginError("Email or password is incorrect.")
-        }
-      })
-  }
+          setAlert({
+            open: true,
+            kind: "error",
+            primary: "Email or password is incorrect.",
+          })
+        })
+    },
+    [client, setAlert, setCustomer, setLoggedIn]
+  )
 
   return { login }
 }
