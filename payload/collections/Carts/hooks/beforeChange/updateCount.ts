7cc13cd9aba592f54d66da49287349e9dac0ff00
Michael Nelson <revnelson@gmail.com>
2023-08-14 23:49:12 -0700

    Prevent cart count from calculating unless needed.

--- payload/collections/Carts/hooks/beforeChange/updateCount.ts
+++ payload/collections/Carts/hooks/beforeChange/updateCount.ts
@@ -1,17 +1,24 @@
-import { CollectionBeforeChangeHook } from "payload/types"
-import { Cart } from "../../../../payload-types"
+import type { CollectionBeforeChangeHook } from "payload/types"
+import type { Cart } from "payload/generated-types"
 
 const updateCountHook: CollectionBeforeChangeHook<Cart> = async ({ data }) => {
-  let count = 0
-  data?.items &&
-    data?.items.length > 0 &&
-    data.items.forEach((item) => {
-      count += item.quantity
-    })
+  if (data.items) {
+    // Items are being updated. Count must be calculated
 
-  const updatedCart = { ...data, count }
+    let count = 0
+    data?.items &&
+      data?.items.length > 0 &&
+      data.items.forEach((item) => {
+        count += item.quantity
+      })
 
-  return updatedCart
+    const updatedCart = { ...data, count }
+
+    return updatedCart
+  } else {
+    // Skip count calculation
+    return data
+  }
 }
 
 export default updateCountHook
