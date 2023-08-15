4752cb2c5bea965931c80a6f00430c94275d838b
Michael Nelson <revnelson@gmail.com>
2023-08-15 13:19:58 -0700

    Add debugging to user access check.

--- payload/collections/Users/access/adminsAndUser.ts
+++ payload/collections/Users/access/adminsAndUser.ts
@@ -4,15 +4,21 @@ import { checkRole } from "../../../access/checkRole"
 import { User } from "../../../payload-types"
 
 const adminsAndUser: Access<User, User> = ({ req: { user } }) => {
+  console.log("User", user)
+
   if (user) {
     if (checkRole(["admin"], user)) {
+      console.log("User verified as administrator")
+
       return true
     }
 
+    console.log("User is not administrator. Checking ID")
     return {
       id: { equals: user.id },
     }
   }
+  console.log("User failed auth checks")
 
   return false
 }
