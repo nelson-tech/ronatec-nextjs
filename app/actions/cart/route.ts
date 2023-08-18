40ac72dec2798c34047723cc242a653f7108c1d7
Michael Nelson <revnelson@gmail.com>
2023-08-17 21:42:12 -0700

    Fix action route security vulnerability.

--- app/actions/cart/route.ts
+++ app/actions/cart/route.ts
@@ -2,9 +2,7 @@ import getPayloadAndUser from "@server/getPayloadAndUser"
 import { NextResponse } from "next/server"
 
 export async function GET(request: Request) {
-  console.log("YASS")
-
   const { payload, user } = await getPayloadAndUser()
 
-  return NextResponse.json({ data: payload.config })
+  return NextResponse.json({})
 }
