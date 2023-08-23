418ce25f73a8e37c8874a5711e37061103660352
Michael Nelson <revnelson@gmail.com>
2023-08-22 19:27:17 -0700

    Add calculateTotals hook to orders.

--- payload/collections/Orders/hooks/beforeChange/index.ts
+++ payload/collections/Orders/hooks/beforeChange/index.ts
@@ -1,7 +1,12 @@
 import { BeforeChangeHook } from "payload/dist/collections/config/types"
 import addOrderNumber from "./addOrderNumber"
 import setPending from "./setPending"
+import calculateTotals from "~payload/hooks/collection/beforeChange/calculateTotals"
 
-const beforeChange: BeforeChangeHook[] = [addOrderNumber, setPending]
+const beforeChange: BeforeChangeHook[] = [
+  addOrderNumber,
+  setPending,
+  calculateTotals,
+]
 
 export default beforeChange
