7e053812c65efad8646aaae4f00ad4ad12b93c93
Michael Nelson <revnelson@gmail.com>
2023-09-04 23:57:24 -0700

    Update debugging logging.

--- src/payload/access/admins.ts
+++ src/payload/access/admins.ts
@@ -6,5 +6,7 @@ import type { User } from "~payload-types"
 type isAdmin = (args: AccessArgs<any, User>) => boolean
 
 export const admins: isAdmin = ({ req: { user } }) => {
+  console.log("Checking admin", user, checkRole(["admin"], user))
+
   return checkRole(["admin"], user)
 }
