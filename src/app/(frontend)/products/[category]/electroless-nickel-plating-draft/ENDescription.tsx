b747983be2fb4d74c87ae75687320cf61aedddc3
Michael Nelson <revnelson@gmail.com>
2023-09-07 14:32:17 -0700

    Add EN draft page.

---
+++ src/app/(frontend)/products/[category]/electroless-nickel-plating-draft/ENDescription.tsx
@@ -0,0 +1,203 @@
+"use client"
+
+import { Disclosure, Transition } from "@headlessui/react"
+
+const ENDescription = () => {
+  return (
+    <div className="prose mt-8 max-w-none">
+      <div>
+        <h2>MACDERMID</h2>
+
+        <h4>NiKlad ELV 849</h4>
+        <p>
+          Outstanding brightness, lead and cadmium-free, NiKlad ELV 849 conforms
+          to ELV, WEEE and ROHS directives. NiKlad 849 provides very good
+          deposit brightness, hardness and wear resistance with a good level of
+          corrosion resistance for an electroless plating coating.
+        </p>
+
+        <h4>NiKlad 767</h4>
+        <p>
+          Our most robust and widely used system. Very bright, consistent high
+          speed bath for general applications with high or low loading. NiKlad
+          767 can be operated without ammonia. This electroless plating bath
+          provides both high hardness and wear resistance with a moderate
+          corrosion resistant coating.
+        </p>
+
+        <h4>NiKlad ICE Ultra</h4>
+        <p>
+          Combines the hardness and uniformity of electroless nickel coating
+          with the lubricity and anti-stick properties of PTFE. The uniform
+          composite of this electroless plating process combined with PTFE
+          particles provides low friction and good release properties. The PTFE
+          content can be modified between 5 - 35% depending on application
+          requirements.
+        </p>
+
+        <h4>NiKlad Eclipse</h4>
+        <p>
+          NiKlad Eclipse is an oxidizing solution designed to give black
+          deposits when used with a duplex electroless coating where the top
+          layer is the NiKlad ELV 824. NiKlad Eclipse process is designed
+          without any heavy metal stabilizers such as lead or cadmium to be
+          compliant with WEEE, RoHS and ELV directives.
+        </p>
+
+        <h4>NiKlad ELV 811</h4>
+        <p>
+          NiKlad ELV 811 provides an excellent corrosion resistant coating, good
+          deposit hardness and wear resistance. Designed to resist strong
+          oxidising conditions by incorporating tin into the electroless plated
+          coating. It can be operated without ammonia.
+        </p>
+
+        <h4>Vand-Aloy 4200</h4>
+        <p>
+          High speed bath with a long service life. It is best suited for use in
+          anodically protected stainless steel and conventional polyproplyene
+          equipment. Vand-Aloy 4200 electroless plating process is tolerant of
+          many impurities and suited for those production facilities where
+          maximum throughput and quality is required. The system can be operated
+          without ammonia.
+        </p>
+
+        <h4>Alumon EN</h4>
+        <p>
+          Cyanide Free dual metal zincate. Alumon EN is specifically designed
+          for plating electroless nickel onto aluminum.
+        </p>
+      </div>
+      <div>
+        <h2>COVENTYA</h2>
+
+        <h4>Enova 509</h4>
+        <p>
+          ENOVA EF-509 is specifically formulated to produce a low stress,
+          semi-bright mid phosphorus (7.5-9.5%) deposit at a consistent rate of
+          deposition. This process does not contain lead or cadmium and meets
+          the requirements of the End-of-Vehicle Life Directive and RoHS.
+        </p>
+
+        <h4>Enova 587 / 790 / 785</h4>
+        <p>
+          ENOVA EF 587 is a lead free, bright, medium phosphorus alloy system
+          for processing ferrous and non-ferrous alloys. This process is created
+          to have very good initiation rates for plating all types of substrates
+          including steel, brass, copper and aluminum. It can provide over 5
+          MTO&apos;s solution life plating Aluminum substrates without the need
+          for an additional costly EN strike chemistry. The resulting deposit
+          provides a more consistent color and appearance with good deposit
+          smoothness over a long solution life.ENOVA EF 587 is designed to
+          deliver a high deposition rate with good throughput while providing
+          exceptionally lasting stability during both bath make up and
+          concentrate shelf life.
+        </p>
+
+        <h4>Enova 949</h4>
+        <p>
+          The ENOVA 949 is specifically formulated to produce a semi-bright,
+          high phosphorous electroless nickel deposit.It&apos;s an excellent
+          high speed production bath offering a 0.5 mil/hr (12 microns/hr) rate
+          of deposition over a wide range of solution loading for both rack or
+          barrel applications. The process yields a deposit with excellent
+          functional properties in terms of ductility, internal stress,
+          solderability and corrosion resistance. It is recommended for
+          applications requiring minimum deposit porosity for corrosive
+          environments.
+        </p>
+
+        <h4>Enova 115</h4>
+        <p>
+          The ENOVA 115 process is an electroless nickel-phosphorus plating
+          system that incorporates polytetrafluoroethylene (PTFE) as occluded
+          particles in a high phosphorus electroless nickel deposit matrix with
+          a capability range from 15 to 25% by volume PTFE inclusion. This
+          unique process is specially designed to produce a consistent amount of
+          uniformly dispersed PTFE throughout the EN deposit and is a RoHS
+          compliant coating. The ENOVA 115 PTFE Dispersion meets PFOS/PFOA free
+          technology meets new regulation environmental demands
+        </p>
+
+        <h4>Enova Nistripr / Metex SCB / EN 86</h4>
+        <p>
+          Metex SCB Nickel Stripper is a two-component system designed to strip
+          dull, semi-bright, bright or duplex nickel electrodeposits, or
+          electroless nickel deposits from copper, brass, steel or assemblies of
+          these materials. The solution will also strip nickel from steel with
+          brazed joints
+        </p>
+      </div>
+      <div>
+        <h2>RONATEC</h2>
+
+        <h4>Ronatec RoHS EN 1000</h4>
+        <p>
+          <strong>EN 1000</strong> is specifically formulated to produce a
+          bright, medium phosphorous, electroless nickel deposit. The system is
+          an excellent high speed production bath offering a 0.7 mil/hr or more
+          rate of deposition over a wide range of solution loading for both rack
+          or barrel applications. The resulting deposit is RoHS compliant and is
+          in accordance with the requirements of REACH
+        </p>
+        <p>
+          <strong>EN 1000</strong> meets end use requirements of AMS 2404,
+          Mil-C-26074, Mil-C-38999, and BAC 5728, DPS 9.67.
+        </p>
+
+        <h4>Ronatec RoHS EN CSB</h4>
+        <p>
+          Ronatec RoHS EN CSB is a semi brite, medium phosphorous electroless
+          nickel process developed to meet the requirements of the electrical
+          connector industry. The deposit is more ductile than a standard,
+          bright medium phosphorous process. This chemistry is suitable for
+          extremely high build applications as well
+        </p>
+
+        <h4>Ronatec RoHS EN 1012</h4>
+        <p>
+          The <strong>RoHS EN 1012</strong> produces a high phosphorous
+          electroless nickel deposit at elevated plating rates (0.40.6 mil per
+          hour). This solution is ideal for critical high volume applications.
+          Connector and Oil and Gas applications are common. The
+          <strong>1012</strong> is also suitable for aerospace hardware.
+        </p>
+        <p>
+          <strong>RoHS EN 1012</strong> deposits meet specification requirements
+          of ASTM B733, AMS 2404, AMS-26074 (formerly Mil-C-26074), ISO 4527 and
+          RoHS Directive 2002/95/EC.
+        </p>
+
+        <h4>Zincate 50</h4>
+        <p>
+          ZINCATE 50 is a cyanide-free dilute zincate process for processing
+          aluminum and its alloys. It removes aluminum oxide and applies a thin
+          zinc film on the aluminum alloy by immersion to prevent its
+          re-oxidation. This film can then be electroplated with copper, nickel
+          and certain other electrolytic processes as well as with electroless
+          nickel. The dilute ZINCATE 50 solution has low viscosity that rapidly
+          penetrates the water film present on the cleaned aluminum. This new
+          alloy process produces a denser, more uniform zinc film that improves
+          adhesion to the aluminum, helping to insure excellent adhesion of the
+          subsequent plate. This unique zincate film cannot be found with most
+          other conventional zincates.
+        </p>
+
+        <h4>Ronatec NIS</h4>
+        <ul>
+          <li>
+            RONATEC NIS is a non-cyanide alkaline stripper that dissolves
+            electroless nickel deposits from steel, copper and copper alloys.
+          </li>
+          <li>No Fe substrate attack</li>
+          <li>No Brass or Copper substrate attack</li>
+          <li>High stripping speed</li>
+          <li>Cyanide Free Process</li>
+          <li>Long-life-Up to 4 oz/gal (30 g/liter) Nickel Metal</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default ENDescription
