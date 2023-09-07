98ccf543ae08cbc7168c87b4caae19028bc31b03
Michael Nelson <revnelson@gmail.com>
2023-09-07 12:55:51 -0700

    Update access logging.

--- src/payload/collections/Carts/index.ts
+++ src/payload/collections/Carts/index.ts
@@ -5,7 +5,6 @@ import beforeChange from "./hooks/beforeChange"
 import afterChange from "./hooks/afterChange"
 import afterDelete from "./hooks/afterDelete"
 import afterRead from "./hooks/afterRead"
-import ProductItemsField from "../../fields/productItems"
 import beforeRead from "./hooks/beforeRead"
 import fields from "./fields"
 
@@ -13,10 +12,10 @@ export const Carts: CollectionConfig = {
   slug: "carts",
   fields,
   access: {
-    read: adminsAndUser("carts", "user.id", "Cart access"),
-    create: adminsAndUser("carts", "user.id", "Cart access"),
-    update: adminsAndUser("carts", "user.id", "Cart access"),
-    delete: adminsAndUser("carts", "user.id", "Cart access"),
+    read: adminsAndUser("carts", "user.id"),
+    create: adminsAndUser("carts", "user.id"),
+    update: adminsAndUser("carts", "user.id"),
+    delete: adminsAndUser("carts", "user.id"),
   },
   hooks: { afterRead, beforeChange, afterChange, afterDelete, beforeRead },
   admin: { group: "Shop", hidden: false },
